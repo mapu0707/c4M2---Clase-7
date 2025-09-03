a24f8d05b8bdbd6d3971123ef5dc92e4a5ca9a5c
Maria Paula Rojas <mariaprojas951@gmail.com>
2025-09-03 18:58:54 -0500

    enums

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        WeekDay day = null;
+        day = WeekDay.FRIDAY;
+        System.out.println(day);
     }
 }
