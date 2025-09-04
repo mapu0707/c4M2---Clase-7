2287842f852ba0fd18cb9e6a4ee779e55fc23b28
Maria Paula Rojas <mariaprojas951@gmail.com>
2025-09-03 19:57:11 -0500

    new commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // demoWeekDay();
+        demoUser();
+    }
+
+    private static void demoUser() {
+        var cesar = new User("cesar", "cesar@email.com", "123456", true, UserRole.ADMIN);
+        var jose = new User("jose", "jose@email.com", "123456");
+        var UserArray = new User[] { cesar, jose };
+
+        for (int i = 0; i < UserArray.length; i++) {
+            var user = UserArray[i];
+            switch (user.getRole()) {
+                case ADMIN:
+                    System.out.printf("%s eres el superusuario%n", user);
+                    break;
+                case CLIENT:
+                    System.out.printf("%s eres el cliente preferido%n", user);
+                    break;
+                case PROVIDER:
+                    System.out.printf("%s eres el proveedor por defecto%n", user);
+                    break;
+                case USER:
+                    System.out.printf("%s ¿Que haces aqui?%n", user);
+                    break;
+                case EMPLOYEE:
+                    System.out.printf("%s ¿No estas trabajando?%n", user);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private static void demoWeekDay() {
         WeekDay day = null;
         day = WeekDay.FRIDAY;
         System.out.println(day);
---
+++ src/UserRole.java
@@ -0,0 +1,5 @@
+
+public enum UserRole {
+    ADMIN, CLIENT, PROVIDER, EMPLOYEE, USER;
+    
+}
